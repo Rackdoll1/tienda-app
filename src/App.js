235d5fd3b702f5ef663ce93d1aea0fe594b4447c
Samara <samarams12@gmail.com>
2021-11-01 18:54:27 -0600

    homebar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './App.css';
-// own scss styles
-import "./styles/organismos/organismos.scss"
+
+import '../src/App.css'
+// importamos Navbar
+import Navbar from './components/Navbar/Navbar'
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
 
 function App() {
   return (
-    <>
+    <Router>
+      <Navbar />
+      {/* exact especificamente */}
+      <Route exact path="/">
+        <h3>Info de home</h3>
+      </Route>
+      <Route exact path="/login">
+        <Login></Login>
+      </Route>
+      <Route exact path="/signup">
+        <Signup></Signup>
+      </Route>
 
-    <h3>que u</h3>
-    </>
-
-
+    </Router>
   );
 }
 
